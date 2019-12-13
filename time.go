ac3ad0feb6b150e4180d72580e853c263cd0c850
Maarten Hoogendoorn <maarten@moretea.nl>
2019-12-13 17:18:26 +0100

    Patch strfmt to support the timestamps in Avaza

--- time.go
+++ time.go
@@ -53,7 +53,8 @@ func IsDateTime(str string) bool {
 
 const (
 	// RFC3339Millis represents a ISO8601 format to millis instead of to nanos
-	RFC3339Millis = "2006-01-02T15:04:05.000Z07:00"
+	RFC3339Millis   = "2006-01-02T15:04:05.000Z07:00"
+	AvazaTimeFormat = "2019-02-11T11:07:42.1233333"
 	// RFC3339Micro represents a ISO8601 format to micro instead of to nano
 	RFC3339Micro = "2006-01-02T15:04:05.000000Z07:00"
 	// ISO8601LocalTime represents a ISO8601 format to ISO8601 in local time (no timezone)
@@ -63,7 +64,7 @@ const (
 )
 
 var (
-	dateTimeFormats = []string{RFC3339Micro, RFC3339Millis, time.RFC3339, time.RFC3339Nano, ISO8601LocalTime}
+	dateTimeFormats = []string{RFC3339Micro, RFC3339Millis, time.RFC3339, time.RFC3339Nano, ISO8601LocalTime, AvazaTimeFormat}
 	rxDateTime      = regexp.MustCompile(DateTimePattern)
 	// MarshalFormat sets the time resolution format used for marshaling time (set to milliseconds)
 	MarshalFormat = RFC3339Millis
